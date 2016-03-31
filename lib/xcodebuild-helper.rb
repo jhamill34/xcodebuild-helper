3871e23e3def1b6a994a579affc3d3548a5e714c
Josh Rasmussen <xlr8runner@gmail.com>
2016-03-30 17:09:44 -0700

    Start creating DSL specs

--- lib/xcodebuild-helper.rb
+++ lib/xcodebuild-helper.rb
@@ -2,4 +2,7 @@ require 'xcode'
 require 'execute'
 
 module XCodeBuildHelper
+  def define(name, &block)
+
+  end
 end
---
+++ spec/xcode_dsl_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe "DSL" do
+  context ".define" do  
+
+  end
+end
