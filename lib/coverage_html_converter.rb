dcbfc3367a1152c6a891547d85c2afab7aed8afc
Joshua Rasmussen <xlr8runner@gmail.com>
2016-04-03 17:25:00 -0700

    Create single file html based off of coverage output

--- lib/coverage_html_converter.rb
+++ lib/coverage_html_converter.rb
@@ -1,7 +1,46 @@
+require 'nokogiri'
+
 module XCodeBuildHelper
+  LINE_NUMBER_CLASS = "num"
+  COVERAGE_CLASS = "coverage"
+  SRC_CLASS = "src"
+  LINE_BREAK_DELIMITER = "\n"
+  LINE_INFO_DELIMITER = "|"
+
   class CoverageHtmlConverter
-    def convert(input)
-      "<tr><td class=\"num\">12</td><td class=\"src\"># some random code written here</td><td class=\"coverage\">0</td></tr>"
+    def convert_file(input)
+      lines = input.split(LINE_BREAK_DELIMITER)
+      title = lines.first
+      lines = lines[1..-1]
+      return Nokogiri::HTML::Builder.new do |doc|
+        doc.html {
+          doc.head {
+
+          }
+
+          doc.body {
+            doc.h1 {
+              doc.text title
+            }
+            doc.table {
+              lines.each do |line|
+                elements = line.split(LINE_INFO_DELIMITER).map { |l| l.strip }
+                doc.tr {
+                  doc.td(:class => LINE_NUMBER_CLASS) {
+                    doc.text elements[1]
+                  }
+                  doc.td(:class => SRC_CLASS) {
+                    doc.text elements[2]
+                  }
+                  doc.td(:class => COVERAGE_CLASS) {
+                    doc.text elements[0]
+                  }
+                }
+              end
+            }
+          }
+        }
+      end
     end
   end
 end
--- spec/coverage_html_coverter_spec.rb
+++ spec/coverage_html_coverter_spec.rb
@@ -1,12 +1,18 @@
 require 'coverage_html_converter'
+require 'nokogiri'
 
 RSpec.describe XCodeBuildHelper::CoverageHtmlConverter do
-  context "#convert" do
-    it "should convert simple text to html" do
+  context "#convert_file" do
+    it "should return a table element" do
       converter = XCodeBuildHelper::CoverageHtmlConverter.new
-      input = "0|  12|# some random code written here"
-      output = converter.convert input
-      expect(output).to eq "<tr><td class=\"num\">12</td><td class=\"src\"># some random code written here</td><td class=\"coverage\">0</td></tr>"
+      input = "TITLE\n0 | 12|# some random code\n1 | 13|# other random code"
+      output = converter.convert_file input
+      document = Nokogiri::HTML(output.to_html)
+
+      # assert the look of the document
+      expect(document.css('body h1').first.to_html).to eq "<h1>TITLE</h1>"
+      expect(document.css('body table tr').length).to eq 2
+      expect(document.css('body table tr').first.to_html.gsub("\n", "")).to eq "<tr><td class=\"num\">12</td><td class=\"src\"># some random code</td><td class=\"coverage\">0</td></tr>"
     end
   end
 end
