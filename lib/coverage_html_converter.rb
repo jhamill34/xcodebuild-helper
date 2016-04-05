05717ff894efd49145185ae817b5034adc44be8f
Joshua Rasmussen <xlr8runner@gmail.com>
2016-04-04 23:27:25 -0700

    Calculate the percent coverage of a file and put it into HTML

--- lib/coverage_html_converter.rb
+++ lib/coverage_html_converter.rb
@@ -8,10 +8,27 @@ module XCodeBuildHelper
   LINE_INFO_DELIMITER = "|"
 
   class CoverageHtmlConverter
+    def self.preprocess_file(lines)
+      relevent_lines = 0.0
+      covered_lines = 0.0
+      lines.each do |line|
+        elements = line.split(LINE_INFO_DELIMITER).map { |l| l.strip }
+        if elements[0] != ""
+          relevent_lines += 1.0
+          covered_lines += 1.0 if elements[0].to_f > 0.0
+        end
+      end
+
+      covered_lines / relevent_lines
+    end
+
     def self.convert_file(input)
       lines = input.split(LINE_BREAK_DELIMITER)
       title = lines.first
       lines = lines[1..-1]
+
+      total_coverage = preprocess_file(lines)
+      coverage_string = (total_coverage * 100).to_s + "%"
       builder = Nokogiri::HTML::Builder.new do |doc|
         doc.html {
           doc.head {
@@ -22,9 +39,13 @@ module XCodeBuildHelper
             doc.h1 {
               doc.text title
             }
+            doc.div(:class => "total_coverage"){
+              doc.text coverage_string
+            }
             doc.table {
               lines.each do |line|
                 elements = line.split(LINE_INFO_DELIMITER).map { |l| l.strip }
+
                 doc.tr {
                   doc.td(:class => LINE_NUMBER_CLASS) {
                     doc.text elements[1]
@@ -42,7 +63,7 @@ module XCodeBuildHelper
         }
       end
 
-      {:content => builder, :title => title}
+      {:content => builder, :title => title, :coverage => total_coverage}
     end
   end
 end
--- spec/coverage_html_coverter_spec.rb
+++ spec/coverage_html_coverter_spec.rb
@@ -9,9 +9,31 @@ RSpec.describe XCodeBuildHelper::CoverageHtmlConverter do
       document = Nokogiri::HTML(output[:content].to_html)
 
       expect(output[:title]).to eq "TITLE"
+      expect(output[:coverage]).to eq 0.5
       # assert the look of the document
       expect(document.css('body h1').first.to_html).to eq "<h1>TITLE</h1>"
+      expect(document.css('body div.total_coverage').first.to_html).to eq "<div class=\"total_coverage\">50.0%</div>"
       expect(document.css('body table tr').length).to eq 2
+      expect(document.css('body table tr').first.to_html.gsub("\n", "")).to eq "<tr><td class=\"num\">12</td><td class=\"src\"># some random code</td><td class=\"coverage\">0</td></tr>"
+    end
+
+    it "should handle rows without coverage" do
+      input = "TITLE\n | 12|# some random code\n 1| 13|# other random code"
+      output = XCodeBuildHelper::CoverageHtmlConverter.convert_file input
+      document = Nokogiri::HTML(output[:content].to_html)
+
+      # assert the look of the document
+      expect(output[:coverage]).to eq 1.0
+      expect(document.css('body table tr').first.to_html.gsub("\n", "")).to eq "<tr><td class=\"num\">12</td><td class=\"src\"># some random code</td><td class=\"coverage\"></td></tr>"
+    end
+
+    it "should handle rows without coverage" do
+      input = "TITLE\n 0| 12|# some random code\n 0| 13|# other random code"
+      output = XCodeBuildHelper::CoverageHtmlConverter.convert_file input
+      document = Nokogiri::HTML(output[:content].to_html)
+
+      # assert the look of the document
+      expect(output[:coverage]).to eq 0.0
       expect(document.css('body table tr').first.to_html.gsub("\n", "")).to eq "<tr><td class=\"num\">12</td><td class=\"src\"># some random code</td><td class=\"coverage\">0</td></tr>"
     end
   end
